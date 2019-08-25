8f29ecc8566f1602dc8941cd83b11631d2e1e815
luciferankon <44019225+luciferankon@users.noreply.github.com>
2019-08-26 00:37:30 +0530

    initial layout of report grades

---
+++ precache-manifest.84fbe1e1c9e5d2bc1f324f6451b19f28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844900ce20a1418b8970069144b24c65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06566a6d8eed45cd22ce",
+    "url": "/static/css/main.18106feb.chunk.css"
+  },
+  {
+    "revision": "5b67e4de97d0443dd5b8",
+    "url": "/static/js/2.501e491a.chunk.js"
+  },
+  {
+    "revision": "06566a6d8eed45cd22ce",
+    "url": "/static/js/main.f03090d9.chunk.js"
+  },
+  {
+    "revision": "f111af055e992897f753",
+    "url": "/static/js/runtime~main.d26d0cbb.js"
+  }
+]);
